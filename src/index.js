38092e3fcb4dad6d81345df605d8e391dfe5db02
Erick Lui <erickjin.lui@gmail.com>
2017-11-29 14:36:55 -0800

    updated index.js

--- src/index.js
+++ src/index.js
@@ -4,20 +4,14 @@ import ReactDOM from 'react-dom';
 import Checkerboard from './components/checkboard';
 
 const App = (props) => {
-  // let num = prompt('Enter number of rows and cols between 1-12');
-  //
-  // if (isNaN(num)) {
-  //   return <div>Please enter a number</div>
-  // } else if (num < 0 || num > 12) {
-  //   return <div>Please enter number between 1-12</div>
-  // }
+  let num = prompt('Enter number of rows and cols between 1-12');
 
-  // const styles = {
-  //   row: {height: '20px'},
-  //   cell: {height: '20px', width: '20px', display:'inline-block'},
-  //   colorA: {backgroundColor: 'black'},
-  //   colorB: {backgroundColor: 'red'}
-  // };
+  if (isNaN(num)) {
+    return <div>Please enter a number</div>
+  } else if (num < 0 || num > 12) {
+    return <div>Please enter number between 1-12</div>
+  }
+
   return (
     <div>
       <Checkerboard/>
